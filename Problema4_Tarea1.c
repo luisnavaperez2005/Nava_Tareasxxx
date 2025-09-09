ea2e3780104c917a72123f92428be684d7061e42
luisnavaperez2005 <luis.nava.perez@uabc.edu.mx>
2025-09-09 13:59:36 -0700

    Verision 1.4.1 Primero incluimos las librerias de stdio.h, aleatorio.h y stdbool.h para poder usar el false y el true. Despues hacemos los prototipos de las funciones que vamos a usar. En este caso declaramos las funciones valorCarta, obtenerPaloAleatorio y para poder usar el true y false usamos la funcion bool esPar para que pueda arrojar la respuesta de la pregunta es par.

---
+++ Problema4_Tarea1.c
@@ -0,0 +1,16 @@
+//
+// Created by NAVA on 09/09/2025.
+//
+#include <stdio.h>
+#include "aleatorio.h"
+#include <stdbool.h>
+
+int valorCarta();
+char obtenerPaloAleatorio();
+bool esPar(int valor1, char palo1, int valor2, char palo2);
+
+int main() {
+    initAleatorio();
+
+    return 0;
+}
