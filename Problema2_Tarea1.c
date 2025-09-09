f6969a077fc201119343851465c0642a67df3930
luisnavaperez2005 <luis.nava.perez@uabc.edu.mx>
2025-09-09 11:29:33 -0700

    Verision 1.2.1 Primero incluimos las librerias de stdio.h y nuestra libreria local aleatorio.h, y despues hacemos el prototipo de nuestra funcion que sera valorCarta. Dentro del main ingresamos la funcion initAleatorio que funcionara como nuestro srand.

---
+++ Problema2_Tarea1.c
@@ -0,0 +1,16 @@
+//
+// Created by NAVA on 09/09/2025.
+//
+
+#include <stdio.h>
+#include "aleatorio.h"
+
+int valorCarta ();//prototipo de nuestra funcion
+
+int main () {
+    initAleatorio();//funcion sacada de aleatorio.c que servira como srand
+
+
+    return 0;
+}
+
