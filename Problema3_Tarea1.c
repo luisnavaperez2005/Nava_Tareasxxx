ad99d02d4987c106fc412981f814bee03e10b56f
luisnavaperez2005 <luis.nava.perez@uabc.edu.mx>
2025-09-09 12:38:57 -0700

    Verision 1.3.2. Despues se ponen las instrucciones de las 3 funciones. La funcion valorCarta funciona para generar el numero de carta aleatoriamente. Dentro de la funcion se declara la variable valor de tipo entero que es igual a la funcion numeroAleatorioConRango que esta en nuestra libreria local aleatorio.h En esta funcion del numero aleatorio ya nos permite poner el rango minimo y maximo para generar el numero de carta. En la funcion obtenerPaloAleatorio funcionara para que pueda generar el palo de la carta generada. Para eso se usa switch (valor) en un rango de 0 a 3 (osea, 4 casos que son los diferentes palos de la baraja inglesa). En el caso 0 el palo sera picos (p), en el caso 1 el paso sera trebor (t), en el caso 2 el palo sera diamantes (d), en el caso 3 el palo sera corazones (c). Y el valor de estos casos se retornara en la variable palo. Y ahora, para la funcion mostrarCarta tambien se usara el valor y el palo que salio anteriormente para que se puedan imprimir dentro de la funcion mostrarCarta. Como queremos que 1 sea A, entonces en el caso 1 se imprimira una A y el palo que genero, en el caso 11 se imprimira una J y el palo que genero, en el caso 12 se imprimira una Q y el palo que genero y en el caso 13 se imprimira una K y el palo que genero. Y en caso de que ninguno de esos numeros sean los que se generaron, simplemente se imprimira el numero de carta y el palo que genero.

--- Problema3_Tarea1.c
+++ Problema3_Tarea1.c
@@ -6,11 +6,60 @@
 #include "aleatorio.h"//libreria aleatorio local
 
 int valorCarta();//Se hace prototipo de la funcion valorCarta
-char obtenerPaloAleatorio;//Se hace prototipo de la funcion obtenerPaloAleatorio
+char obtenerPaloAleatorio();//Se hace prototipo de la funcion obtenerPaloAleatorio
 void mostrarCarta (int valor, char palo);//Se hace prototipo de la nueva funcion mostrarCarta
 
 int main () {
     initAleatorio();// Funcion sacada de nuestra libreria local aleatorio que servira como srand
 
+
     return 0;
 }
+
+int valorCarta() {
+    int valor = numeroAleatorioConRango(1, 13);  // 1 a 13
+    return valor;
+}
+
+char obtenerPaloAleatorio() {
+    int valor = numeroAleatorio(3);  // 0 a 3
+    char palo;
+
+    switch (valor) {
+        case 0:
+            palo = 'p';
+            break;
+        case 1:
+            palo = 't';
+            break;
+        case 2:
+            palo = 'd';
+            break;
+        case 3:
+            palo ='c';
+            break;
+    }
+    return palo;
+}
+
+void mostrarCarta(int valor, char palo) {
+    switch (valor) {
+        case 1:
+            printf("A %c\n", palo);
+            break;
+        case 11:
+            printf("J %c\n", palo);
+            break;
+        case 12:
+            printf("Q %c\n", palo);
+            break;
+        case 13:
+            printf("K %c\n", palo);
+            break;
+        default:
+            printf("%d %c\n", valor, palo);
+            break;
+    }
+}
+
+
