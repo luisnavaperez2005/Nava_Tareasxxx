9d114b14ad89f3db4e4b6599688a27d2013fb578
luisnavaperez2005 <luis.nava.perez@uabc.edu.mx>
2025-09-09 10:08:36 -0700

    Verision 1.1.2 Se agrego un switch que dependera de que numero tendra valor. Si el valor es 0 palo sera p, si el valor es 1 palo sera t, si el valor es 2 palo sera d, si valor es 3 palo sera c. Y retornara estos valores de palo a palo.

--- Problema1_Tarea1.c
+++ Problema1_Tarea1.c
@@ -13,4 +13,23 @@ int main () {
     return 0;
 }
 
-
+char obtenerPaloAleatorio () {
+    int valor = 1;
+    char palo;
+    valor = numeroAleatorio(4);//Se llama la funcion de "aleatorio.h" que es rand con maximo 4
+    switch (valor) {
+        case 0:
+            palo = 'p';
+            break;
+        case 1:
+            palo = 't';
+            break;
+        case 2:
+            palo = 'd';
+            break;
+        case 3:
+            palo = 'c';
+            break;
+    }
+    return palo;
+}
