8dd8d107d92b82fe6a2b527ac2e4873e506d7a37
luisnavaperez2005 <luis.nava.perez@uabc.edu.mx>
2025-09-09 12:03:16 -0700

    Verision 1.3.1 Primero inclumos las librerias de stdio.h y la libreria local aleatorio.h. Despues el prototipo de las 2 ultimas funciones que usamos en el problema anterior, que fueron valorCarta obtenerPaloAleatorio y haremos otro prototipo de una nueva funcion que seria mostrarCarta. Ahora, dentro del main usaremos initAleatorio que servira como srand en nuestro codigo.

---
+++ Problema3_Tarea1.c
@@ -0,0 +1,16 @@
+//
+// Created by NAVA on 09/09/2025.
+//
+
+#include <stdio.h>
+#include "aleatorio.h"//libreria aleatorio local
+
+int valorCarta();//Se hace prototipo de la funcion valorCarta
+char obtenerPaloAleatorio;//Se hace prototipo de la funcion obtenerPaloAleatorio
+void mostrarCarta (int valor, char palo);//Se hace prototipo de la nueva funcion mostrarCarta
+
+int main () {
+    initAleatorio();// Funcion sacada de nuestra libreria local aleatorio que servira como srand
+
+    return 0;
+}
