2a8ab11d2911dfe74f16b3a94683801260ba4dc7
luisnavaperez2005 <luis.nava.perez@uabc.edu.mx>
2025-09-09 10:07:01 -0700

    Verision 1.1.1 Se agrego las libreria de "stdio.h" y la libreria de local de "aleatorio.h". Despues se hace el prototipo del palo aleatorio que es "char obtenerPaloAleatorio" Despues, en el main se llama la funcion initAleatorio que funciona como srand. Y por ultimo, se desarrolla la funcion de obtenerPaloAleatorio. Primero declaramos la variable como int y es igual a 1. Despues declaramos la variable palo de tipo char. Y por ultimo le asignamos el valor del numero aleatorio con la funcion numeroAleatorio con un maximo de 4.

---
+++ Problema1_Tarea1.c
@@ -0,0 +1,16 @@
+//
+// Created by NAVA on 09/09/2025.
+//
+
+#include <stdio.h>
+#include "aleatorio.h"
+
+char obtenerPaloAleatorio ();//Se hace el prototipo de la Funcion
+
+int main () {
+    initAleatorio();//Se llama la funcion de "aleatorio.h" que es srand
+
+    return 0;
+}
+
+
